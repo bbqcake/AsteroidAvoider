959079fcbd723b8d9bf38d348ea9828a66fce84f
bbqcake <tdarkture@gmail.com>
2021-07-13 00:23:49 +0200

    Added screen teleport from top to bottom

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,6 +22,22 @@ public class PlayerMovement : MonoBehaviour
     
     void Update()
     {
+        ProcessInput();
+
+        KeepPlayerOnScreen();
+    }
+
+    void FixedUpdate()
+    {
+        if (movementDirection == Vector3.zero) { return; }
+
+        rb.AddForce(movementDirection * forceMagnitude * Time.deltaTime, ForceMode.Force);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity); // prevent it from going too fast
+        
+    }
+
+    private void ProcessInput()
+    {
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
            Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
@@ -39,12 +55,32 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    private void KeepPlayerOnScreen()
     {
-        if (movementDirection == Vector3.zero) { return; }
+        Vector3 newPosition = transform.position;
+        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
 
-        rb.AddForce(movementDirection * forceMagnitude * Time.deltaTime, ForceMode.Force);
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity); // prevent it from going too fast
-        
+        if(viewportPosition.x > 1) // right side of screen
+        {
+            newPosition.x = -newPosition.x + 0.1f; // add on a slight value so we dont teleport back
+        }
+
+        else if(viewportPosition.x < 0) // left
+        {
+            newPosition.x = -newPosition.x - 0.1f; // add on a slight value so we dont teleport back
+        }
+
+        if(viewportPosition.y > 1) // top
+        {
+            newPosition.y = -newPosition.y + 0.1f; // add on a slight value so we dont teleport back
+        }
+
+        else if(viewportPosition.y < 0) // bottom of screen
+        {
+            newPosition.y = -newPosition.y - 0.1f; // add on a slight value so we dont teleport back
+        }
+
+        transform.position = newPosition;
     }
+
 }
